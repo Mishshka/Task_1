a2c4b632f4698c7be33fca8c180c5594796a737c
Mikhail <vbif1907@yandex.ru>
2025-09-25 16:09:35 +0300

    добавление первого выбора

--- program_1.h
+++ program_1.h
@@ -5,7 +5,17 @@
 #include <Windows.h>
 #include <vector>
 #include <string>
+#include <conio.h>
+#include <chrono>
+#include <thread>
 
 // подключение пространства имен
 using namespace std;
 namespace fs = filesystem;
+
+//константы с возможными символами
+const string ENG_let = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const string RUS_let = "јЅ¬√ƒ≈®∆«»… ЋћЌќѕ–—“”‘’÷„ЎўЏџ№ёяaабвгдеЄжзийклмнопрстуфхцчшщъыью€";
+const string num = "0123456789";
+const string SPec = "~!@#$%^&*()+`'\"; :<> / \ |";
+
